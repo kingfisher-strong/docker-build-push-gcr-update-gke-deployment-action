094f37b4b54323282b367c54d57a281dba3312f1
shashank patel <snk0202@gmail.com>
2020-03-27 03:30:04 +0530

    adding gcloud auth

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,3 +3,7 @@
 echo "Hello $1"
 time=$(date)
 echo "::set-output name=time::$time"
+
+set -e
+echo "${INPUT_SERVICE_ACCOUNT}" | base64 --decode > "${HOME}/gcloud.json" 
+gcloud auth activate-service-account --key-file="${HOME}/gcloud.json"
