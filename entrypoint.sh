a7855039b46dbb5f6d27aa30b9c2708c5228ef6e
shashank patel <snk0202@gmail.com>
2020-03-28 12:05:43 +0530

    fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ echo "${INPUT_SERVICE_ACCOUNT}" | base64 -d > "${HOME}/gcloud.json"
 gcloud auth activate-service-account --key-file="${HOME}/gcloud.json"
 
 gcloud auth configure-docker
-docker build -t "${INPUT_REGISTRY}/${PROJECT_ID}/${IMAGE_NAME}" .
-docker push "${INPUT_REGISTRY}/${INPUT_NAME}"
+docker build -t "${INPUT_REGISTRY}/${INPUT_PROJECT_ID}/${INPUT_IMAGE_NAME}" .
+docker push "${INPUT_REGISTRY}/${INPUT_PROJECT_ID}/${INPUT_IMAGE_NAME}"
 
 gcloud container clusters get-credentials "${INPUT_CLUSTER}" --zone "${INPUT_ZONE}" --project "${INPUT_PROJECT_ID}" && gcloud components install kubectl && kubectl -n "${INPUT_NAMESPACE}" set image deployment/"${INPUT_DEPLOYMENT}" "${INPUT_CONTAINER}"="${INPUT_REGISTRY}/${INPUT_NAME}"
