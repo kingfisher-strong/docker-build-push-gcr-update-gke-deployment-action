697b38bcb9b61690ff6ccb77ba815947c5b421ee
shashank patel <snk0202@gmail.com>
2020-03-28 12:10:19 +0530

    fix image name kubectl

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,4 +10,4 @@ gcloud auth configure-docker
 docker build -t "${INPUT_REGISTRY}/${INPUT_PROJECT_ID}/${INPUT_IMAGE_NAME}" .
 docker push "${INPUT_REGISTRY}/${INPUT_PROJECT_ID}/${INPUT_IMAGE_NAME}"
 
-gcloud container clusters get-credentials "${INPUT_CLUSTER}" --zone "${INPUT_ZONE}" --project "${INPUT_PROJECT_ID}" && gcloud components install kubectl && kubectl -n "${INPUT_NAMESPACE}" set image deployment/"${INPUT_DEPLOYMENT}" "${INPUT_CONTAINER}"="${INPUT_REGISTRY}/${INPUT_NAME}"
+gcloud container clusters get-credentials "${INPUT_CLUSTER}" --zone "${INPUT_ZONE}" --project "${INPUT_PROJECT_ID}" && gcloud components install kubectl && kubectl -n "${INPUT_NAMESPACE}" set image deployment/"${INPUT_DEPLOYMENT}" "${INPUT_CONTAINER}"="${INPUT_REGISTRY}/${INPUT_PROJECT_ID}/${INPUT_IMAGE_NAME}"
